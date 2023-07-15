a00b0e9822d71357012d5ed70e2347d9e4f11107
AsijitM <asijit00@gmail.com>
2023-07-15 21:43:01 +0530

    billboard struct

--- app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
+import { useOrigin } from '@/hooks/use-origin';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Store } from '@prisma/client';
 
@@ -38,6 +39,7 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
