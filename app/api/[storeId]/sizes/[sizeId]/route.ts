091825548b04ec7f2b3c5756fc13ae3ceba28451
AsijitM <asijit00@gmail.com>
2023-07-26 19:35:06 +0530

    size section created

---
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -0,0 +1,118 @@
+//patch route for updation
+// delele route for deletion
+
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  _req: Request,
+  //dont delete the req just because it is not used , params can be only  used on the second arguement thus we need to keep the both of them
+  { params }: { params: { sizeId: string } }
+) {
+  try {
+    if (!params.sizeId) {
+      return new NextResponse(' sizeId is required', { status: 400 });
+    }
+
+    const size = await prismadb.size.findUnique({
+      where: {
+        id: params.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log('[Size_GET]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { sizeId: string; storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, value } = body;
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse('Label is required', { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse(' Image url is required', { status: 400 });
+    }
+    if (!params.sizeId) {
+      return new NextResponse(' sizeId is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorised');
+    }
+    const size = await prismadb.size.updateMany({
+      where: {
+        id: params.sizeId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log('[Sizes_PATCH]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  //dont delete the req just because it is not used , params can be only  used on the second arguement thus we need to keep the both of them
+  { params }: { params: { storeId: string; sizeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!params.sizeId) {
+      return new NextResponse(' sizeId is required', { status: 400 });
+    }
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorised');
+    }
+
+    const size = await prismadb.size.deleteMany({
+      where: {
+        id: params.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log('[SIZE_DELETE]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
